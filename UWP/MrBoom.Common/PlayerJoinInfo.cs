75d6f5fb814b922ad04129245b9d0ba227fd53cb
Timofei Zhakov <tima@chemodax.net>
2025-02-24 19:31:09 +0100

    add some default usings

--- UWP/MrBoom.Common/PlayerJoinInfo.cs
+++ UWP/MrBoom.Common/PlayerJoinInfo.cs
@@ -1,5 +1,10 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
 namespace MrBoom.Common
 {
     public class PlayerJoinInfo
