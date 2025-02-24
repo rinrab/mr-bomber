0ef63b8a3e679cd9041e2c9084103a357bdd9749
Timofei Zhakov <tima@chemodax.net>
2025-02-24 18:08:30 +0100

    add empty asp.net core website, to act as the server

---
+++ UWP/MrBoom.Server/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+namespace MrBoom.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
