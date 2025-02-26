9cf33d08999d7827ffbb001a80cdf14338aa72cf
Timofei Zhakov <tima@chemodax.net>
2025-02-26 20:06:18 +0100

    add NetworkException

---
+++ UWP/MrBoom.NetworkProtocol/NetworkException.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrBoom.NetworkProtocol.Messages
+{
+    public class NetworkException : Exception
+    {
+        public NetworkException() : base("Network exception occurred")
+        {
+        }
+    }
+}
