7ce535195650d986db1b032e0161b31dc10f7fae
Timofei Zhakov <tima@chemodax.net>
2025-02-26 20:07:00 +0100

    add tests on encoding

---
+++ UWP/MrBoom.NetworkProtocol.Tests/EncodingTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using MrBoom.Common;
+using MrBoom.Common.Messages;
+
+namespace MrBoom.NetworkProtocol.Tests
+{
+    public class EncodingTests
+    {
+        [Test]
+        public void PlayerJoinPacketMessage()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var msg = new PlayerJoin
+                {
+                    Name = "sigma"
+                };
+                var packet = new Packet(msg);
+                packet.WriteTo(new BinaryWriter(stream));
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var packet2 = new Packet();
+                packet2.ReadFrom(new BinaryReader(stream));
+
+                Assert.AreEqual(msg.Name, ((PlayerJoin)packet2.Message).Name);
+            }
+        }
+    }
+}
