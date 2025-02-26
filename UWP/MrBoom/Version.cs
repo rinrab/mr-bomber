2233e1001bad2c2105ed3f3315abbc5caf52400f
Timofei Zhakov <tima@chemodax.net>
2025-02-26 15:50:01 +0100

    access version throgh a static class version

---
+++ UWP/MrBoom/Version.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using Windows.ApplicationModel;
+
+namespace MrBoom
+{
+    public static class Version
+    {
+        public static readonly string VersionString =
+            $"{Package.Current.Id.Version.Major}." +
+            $"{Package.Current.Id.Version.Minor}." +
+            $"{Package.Current.Id.Version.Build}";
+    }
+}
