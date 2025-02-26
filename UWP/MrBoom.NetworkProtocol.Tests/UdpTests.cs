5cfa93cf8c1bc0031ea2a38b474975cdb4624e51
Timofei Zhakov <tima@chemodax.net>
2025-02-26 20:48:34 +0100

    add udp request tester

---
+++ UWP/MrBoom.NetworkProtocol.Tests/UdpTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MrBoom.Common.Messages;
+using MrBoom.Common;
+using System.Net.Sockets;
+
+namespace MrBoom.NetworkProtocol.Tests
+{
+    public class UdpTests
+    {
+        [Test]
+        public async Task JoinRequester()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var msg = new PlayerJoin
+                {
+                    Name = "yemete kudasai"
+                };
+                var packet = new Packet(msg);
+                packet.WriteTo(new BinaryWriter(stream));
+
+                var client = new UdpClient(0);
+                client.Connect("localhost", 5297);
+
+                await client.SendAsync(stream.GetBuffer(), (int)stream.Length);
+            }
+        }
+    }
+}
