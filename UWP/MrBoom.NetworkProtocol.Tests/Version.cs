5c2178b05e15ccf7e94d87a94e765461ad83cae4
Timofei Zhakov <tima@chemodax.net>
2025-02-27 22:38:35 +0100

    reference everything from the test

--- UWP/MrBoom.NetworkProtocol.Tests/Version.cs
+++ UWP/MrBoom.NetworkProtocol.Tests/Version.cs
@@ -4,6 +4,6 @@ namespace MrBoom
 {
     public static class Version
     {
-        public static readonly string VersionString = "MrBoom test-suite unknown.unknown.unknown";
+        public static readonly string VersionString = "MrBoom Server unknown.unknown.unknown";
     }
 }
