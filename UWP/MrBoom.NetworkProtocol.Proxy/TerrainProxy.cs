e41e7093f79784b013cfcda98c10bccc0b943bc7
Timofei Zhakov <tima@chemodax.net>
2025-02-28 18:16:15 +0100

    add MrBoom.NetworkProtocol.Proxy

---
+++ UWP/MrBoom.NetworkProtocol.Proxy/TerrainProxy.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System.Collections.Generic;
+using MrBoom.Core.Terrain;
+using MrBoom.NetworkProtocol.Messages;
+
+namespace MrBoom.NetworkProtocol.Proxy
+{
+    public class TerrainProxy : ITerrainProxy
+    {
+        public GameInfo Message;
+
+        public int TimeLeft { get; set; }
+        public int ApocalypseSpeed { get; set; }
+        public int MaxApocalypse { get; set; }
+        public int Width => Message.Terrain.Width;
+        public int Height => Message.Terrain.Width;
+        public int LevelIndex => Message.LevelIndex;
+
+        public IList<ISprite> Sprites { get; }
+
+        public TerrainProxy()
+        {
+            Sprites = new List<ISprite>();
+        }
+
+        public void ClientUpdate()
+        {
+        }
+
+        public Cell GetCell(int x, int y)
+        {
+            var cell = Message.Terrain.Grid[x, y];
+
+            if (cell == null)
+            {
+                return new Cell(TerrainType.PermanentWall);
+            }
+            else
+            {
+                return new Cell(cell.Type)
+                {
+                };
+            }
+        }
+
+        public bool IsWalkable(int x, int y)
+        {
+            Cell cell = GetCell(x, y);
+
+            switch (cell.Type)
+            {
+                case TerrainType.Free:
+                case TerrainType.PowerUpFire:
+                    return true;
+
+                case TerrainType.PermanentWall:
+                case TerrainType.Rubber:
+                case TerrainType.Apocalypse:
+                    return false;
+
+                case TerrainType.TemporaryWall:
+                case TerrainType.Bomb:
+                    return false; // cheats.noClip
+
+                default:
+                    return true;
+            }
+        }
+    }
+}
--- UWP/MrBoom/Screens/OnlineGameScreen.cs
+++ UWP/MrBoom/Screens/OnlineGameScreen.cs
@@ -1,79 +1,15 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Graphics;
-using MrBoom.Core.Terrain;
 using MrBoom.NetworkProtocol.Messages;
+using MrBoom.NetworkProtocol.Proxy;
 
 namespace MrBoom.Screens
 {
-    public class TerrainProxy : ITerrainProxy
-    {
-        public GameInfo Message;
-
-        public int TimeLeft { get; set; }
-        public int ApocalypseSpeed { get; set; }
-        public int MaxApocalypse { get; set; }
-        public int Width => Message.Terrain.Width;
-        public int Height => Message.Terrain.Width;
-        public int LevelIndex => Message.LevelIndex;
-
-        public IList<ISprite> Sprites { get; }
-
-        public TerrainProxy()
-        {
-            Sprites = new List<ISprite>();
-        }
-
-        public void ClientUpdate()
-        {
-        }
-
-        public Cell GetCell(int x, int y)
-        {
-            var cell = Message.Terrain.Grid[x, y];
-
-            if (cell == null)
-            {
-                return new Cell(TerrainType.PermanentWall);
-            }
-            else
-            {
-                return new Cell(cell.Type)
-                {
-                };
-            }
-        }
-
-        public bool IsWalkable(int x, int y)
-        {
-            Cell cell = GetCell(x, y);
-
-            switch (cell.Type)
-            {
-                case TerrainType.Free:
-                case TerrainType.PowerUpFire:
-                    return true;
-
-                case TerrainType.PermanentWall:
-                case TerrainType.Rubber:
-                case TerrainType.Apocalypse:
-                    return false;
-
-                case TerrainType.TemporaryWall:
-                case TerrainType.Bomb:
-                    return false; // cheats.noClip
-
-                default:
-                    return true;
-            }
-        }
-    }
-
     public class OnlineGameScreen : ClientGameScreen
     {
         private readonly MultiplayerClient multiplayerClient;
