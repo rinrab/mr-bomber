1e243b5a4ce46dfc58c440a8081c13d41bde776a
Timofei Zhakov <tima@chemodax.net>
2025-02-28 19:13:07 +0100

    organize

--- UWP/MrBoom.Server/LobbyJoinState.cs
+++ UWP/MrBoom.Server/LobbyJoinState.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
-using System;
 using System.Net;
-using MrBoom.NetworkProtocol;
 using MrBoom.NetworkProtocol.Messages;
 
 namespace MrBoom.Server
@@ -85,85 +83,11 @@ namespace MrBoom.Server
 
             if (startIn == 0)
             {
-                state.SetState(new GameLobby(state, logger, clients, players));
+                state.SetState(new LobbyPlayState(state, logger, clients, players));
             }
             else if (startIn > 0)
             {
                 startIn--;
-            }
-        }
-    }
-
-    public class GameLobby : ILobbyState
-    {
-        private readonly ILobbyStateManager state;
-        private readonly ILogger logger;
-        private readonly List<ClientInfo> clients;
-        private readonly List<LobbyPlayer> players;
-
-        public Terrain Terrain { get; }
-
-        public GameLobby(ILobbyStateManager state, ILogger logger, List<ClientInfo> clients, List<LobbyPlayer> players)
-        {
-            this.state = state;
-            this.logger = logger;
-            this.clients = clients;
-            this.players = players;
-
-            Terrain = new Terrain(1);
-        }
-
-
-        public void ServerUpdate()
-        {
-        }
-
-        public void OnPacketReceived(Packet packet, IPEndPoint endPoint)
-        {
-        }
-
-        private IMessage FormatGameInfoMessage()
-        {
-            var grid = new Grid<GameCellInfo>(Terrain.Width, Terrain.Height);
-            for (int i = 0; i < grid.CellCount; i++)
-            {
-                Cell cell = Terrain.GetCell(grid.GetCellX(i), grid.GetCellY(i));
-
-                grid[i] = new GameCellInfo
-                {
-                    Type = cell.Type,
-                };
-            }
-
-            var sprites = new List<GameSpriteInfo>();
-            foreach (Sprite sprite in Terrain.GetSprites())
-            {
-                sprites.Add(new GameSpriteInfo
-                {
-                    X = sprite.X,
-                    Y = sprite.Y,
-                });
-            }
-
-            return new GameInfo
-            {
-                LevelIndex = Terrain.LevelIndex,
-                Terrain = new GameTerrainInfo
-                {
-                    Width = Terrain.Width,
-                    Height = Terrain.Height,
-                    Grid = grid,
-                },
-                Sprites = sprites,
-            };
-        }
-
-        public async Task SendPackets(IUdpServer udpServer, CancellationToken stoppingToken)
-        {
-            foreach (ClientInfo client in clients)
-            {
-                await udpServer.SendPacket(new Packet(FormatGameInfoMessage()),
-                                           client.IpAddress, stoppingToken);
             }
         }
     }
---
+++ UWP/MrBoom.Server/LobbyPlayState.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using System.Net;
+using MrBoom.NetworkProtocol;
+using MrBoom.NetworkProtocol.Messages;
+
+namespace MrBoom.Server
+{
+    public class LobbyPlayState : ILobbyState
+    {
+        private readonly ILobbyStateManager state;
+        private readonly ILogger logger;
+        private readonly List<ClientInfo> clients;
+        private readonly List<LobbyPlayer> players;
+
+        public Terrain Terrain { get; }
+
+        public LobbyPlayState(ILobbyStateManager state, ILogger logger, List<ClientInfo> clients, List<LobbyPlayer> players)
+        {
+            this.state = state;
+            this.logger = logger;
+            this.clients = clients;
+            this.players = players;
+
+            Terrain = new Terrain(1);
+        }
+
+        public void ServerUpdate()
+        {
+        }
+
+        public void OnPacketReceived(Packet packet, IPEndPoint endPoint)
+        {
+        }
+
+        private IMessage FormatGameInfoMessage()
+        {
+            var grid = new Grid<GameCellInfo>(Terrain.Width, Terrain.Height);
+            for (int i = 0; i < grid.CellCount; i++)
+            {
+                Cell cell = Terrain.GetCell(grid.GetCellX(i), grid.GetCellY(i));
+
+                grid[i] = new GameCellInfo
+                {
+                    Type = cell.Type,
+                };
+            }
+
+            var sprites = new List<GameSpriteInfo>();
+            foreach (Sprite sprite in Terrain.GetSprites())
+            {
+                sprites.Add(new GameSpriteInfo
+                {
+                    X = sprite.X,
+                    Y = sprite.Y,
+                });
+            }
+
+            return new GameInfo
+            {
+                LevelIndex = Terrain.LevelIndex,
+                Terrain = new GameTerrainInfo
+                {
+                    Width = Terrain.Width,
+                    Height = Terrain.Height,
+                    Grid = grid,
+                },
+                Sprites = sprites,
+            };
+        }
+
+        public async Task SendPackets(IUdpServer udpServer, CancellationToken stoppingToken)
+        {
+            foreach (ClientInfo client in clients)
+            {
+                await udpServer.SendPacket(new Packet(FormatGameInfoMessage()),
+                                           client.IpAddress, stoppingToken);
+            }
+        }
+    }
+}
