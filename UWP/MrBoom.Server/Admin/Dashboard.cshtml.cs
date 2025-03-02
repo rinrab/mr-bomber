736cdc26eaa7fc72a98f9ff5297e98a16f6c4fec
Timofei Zhakov <tima@chemodax.net>
2025-03-02 21:55:28 +0100

    add pages with the stats

---
+++ UWP/MrBoom.Server/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MrBoom.Server.Admin
+{
+    public class DashboardModel : PageModel
+    {
+        public readonly IMetrics Metrics;
+
+        public DashboardModel(IMetrics metrics)
+        {
+            this.Metrics = metrics;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
