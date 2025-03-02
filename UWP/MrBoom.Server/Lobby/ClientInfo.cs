6d8cd12e3846db5f19098fc33e63b15fe50e4478
Timofei Zhakov <tima@chemodax.net>
2025-03-02 14:10:46 +0100

    ClientInfo: add CorishInfo

--- UWP/MrBoom.Server/Lobby/ClientInfo.cs
+++ UWP/MrBoom.Server/Lobby/ClientInfo.cs
@@ -1,12 +1,16 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
 using System.Net;
+using MrBoom.Core;
+using MrBoom.NetworkProtocol.Proxy;
 
 namespace MrBoom.Server.Lobby
 {
     public class ClientInfo
     {
         public Guid ClientSecret { get; set; }
+
+        public IClientInfo CorishInfo => new ClientInfoGuid(ClientSecret);
 
         public IPEndPoint IpAddress { get; set; }
     }
