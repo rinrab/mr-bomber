25fa87b68728bba77d9208c91e9cb7e126f39da2
Timofei Zhakov <tima@chemodax.net>
2025-03-04 11:34:30 +0100

    add update.sh scrript

---
+++ UWP/MrBoom.Server/update.sh
@@ -0,0 +1,12 @@
+﻿#! /bin/bash
+
+systemctl stop mr-bomber
+git pull
+dotnet publish -o /srv/mr-bomber
+systemctl start mr-bomber
+
+systemctl status mr-bomber
+
+sleep 1s
+
+systemctl status mr-bomber
