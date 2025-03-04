a517466d0aa0ffb7db7c610507e1fd1e8119d522
Timofei Zhakov <tima@chemodax.net>
2025-03-04 19:40:12 +0100

    move ClientSpriteLocalHuman to a new file

--- UWP/MrBoom/ClientSprite.cs
+++ UWP/MrBoom/ClientSprite.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
-using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MrBoom.Core.Sprite;
@@ -88,91 +87,6 @@ namespace MrBoom
                     img.Draw(ctx, x, y, color);
                 }
             }
-        }
-    }
-
-    public class ClientSpriteLocalHuman : Sprite, IClientSprite
-    {
-        private readonly IPlayerProxy proxy;
-        private readonly IClientSprite client;
-        public readonly IController Controller;
-
-        public override bool HasUnplugin => proxy.HasUnplugin;
-        public override bool HasSkull => proxy.HasSkull;
-
-        public override SpriteType Type => proxy.Type;
-        public override int SubType => proxy.SubType;
-
-        public ClientSpriteLocalHuman(ITerrainAccessor terrain, int x, int y,
-                                      IPlayerProxy proxy, Assets assets,
-                                      IController controller) : base(terrain, x, y, 3)
-        {
-            this.proxy = proxy;
-            client = new ClientSprite(this, assets);
-            Controller = controller;
-        }
-
-        private Directions? GetDirection()
-        {
-            if (Controller.IsKeyDown(PlayerKeys.Up))
-            {
-                return Directions.Up;
-            }
-            else if (Controller.IsKeyDown(PlayerKeys.Left))
-            {
-                return Directions.Left;
-            }
-            else if (Controller.IsKeyDown(PlayerKeys.Right))
-            {
-                return Directions.Right;
-            }
-            else if (Controller.IsKeyDown(PlayerKeys.Down))
-            {
-                return Directions.Down;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public void ClientUpdate()
-        {
-            Direction = GetDirection();
-
-            Features = proxy.Features;
-            Skull = proxy.Skull;
-
-            if (Math.Abs(X - proxy.X) + Math.Abs(Y - proxy.Y) > 16)
-            {
-                MoveTo(proxy.X, proxy.Y);
-            }
-
-            base.ServerUpdate();
-
-            if (Controller.IsKeyDown(PlayerKeys.Bomb))
-            {
-                proxy.ToggleDropBomb();
-            }
-
-            if (Controller.IsKeyDown(PlayerKeys.RcDitonate))
-            {
-                proxy.ToggleRemoteControl();
-            }
-
-            proxy.MoveTo(X, Y);
-
-            client.ClientUpdate();
-        }
-
-        public void Draw(SpriteBatch ctx)
-        {
-            client.Draw(ctx);
-        }
-
-        public override void KickBomb(int x, int y, int dx, int dy)
-        {
-            // nothing to kick yet
         }
     }
 }
