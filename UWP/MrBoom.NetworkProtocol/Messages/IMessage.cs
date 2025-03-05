54d1c07d03b49e07c456894670b8478fe9eb04ea
Timofei Zhakov <tima@chemodax.net>
2025-03-05 15:30:01 +0100

    add IKeyedMessage and IKeyedMessageCollection interfaces

--- UWP/MrBoom.NetworkProtocol/Messages/IMessage.cs
+++ UWP/MrBoom.NetworkProtocol/Messages/IMessage.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace MrBoom.NetworkProtocol.Messages
 {
@@ -6,5 +7,16 @@ namespace MrBoom.NetworkProtocol.Messages
     {
         void ReadFrom(BinaryReader reader);
         void WriteTo(BinaryWriter writer);
+    }
+
+    public interface IKeyedMessage<KeyT> : IMessage
+    {
+        KeyT Key { set; get; }
+    }
+
+    public interface IKeyedMessageCollection<KeyT, ValueT>
+        where ValueT : IKeyedMessage<KeyT>, IMessage
+    {
+        IList<ValueT> Children { get; }
     }
 }
