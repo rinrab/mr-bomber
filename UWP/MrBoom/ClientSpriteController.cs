4d45127224dfd79d3a165ed1401176e543f18002
Timofei Zhakov <tima@chemodax.net>
2025-03-15 18:26:43 +0100

    add ClientSpriteController and ClientSpriteGraphics modules

---
+++ UWP/MrBoom/ClientSpriteController.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using MrBoom.Core.Sprites;
+using MrBoom.Core.Terrain;
+
+namespace MrBoom
+{
+    public class ClientSpriteController : IServerGameEntity
+    {
+        private readonly IController controller;
+        private readonly SpritePosition position;
+        private readonly SpriteMovementController movementController;
+        private readonly SpriteSpeedProvider speedProvider;
+        private readonly IPlayerProxy proxy;
+
+        public ClientSpriteController(IController controller,
+                                      SpritePosition position,
+                                      SpriteMovementController movementController,
+                                      SpriteSpeedProvider speedProvider,
+                                      IPlayerProxy proxy)
+        {
+            this.controller = controller;
+            this.position = position;
+            this.movementController = movementController;
+            this.speedProvider = speedProvider;
+            this.proxy = proxy;
+        }
+
+        private Directions? GetDirection()
+        {
+            if (controller.IsKeyDown(PlayerKeys.Up))
+            {
+                return Directions.Up;
+            }
+            else if (controller.IsKeyDown(PlayerKeys.Left))
+            {
+                return Directions.Left;
+            }
+            else if (controller.IsKeyDown(PlayerKeys.Right))
+            {
+                return Directions.Right;
+            }
+            else if (controller.IsKeyDown(PlayerKeys.Down))
+            {
+                return Directions.Down;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void ServerUpdate()
+        {
+            movementController.Move(GetDirection(), speedProvider.ProvideActualSpeed());
+
+            if (Math.Abs(position.X - proxy.X) + Math.Abs(position.Y - proxy.Y) > 16)
+            {
+                position.MoveTo(proxy.X, proxy.Y);
+            }
+
+            if (controller.IsKeyDown(PlayerKeys.Bomb))
+            {
+                proxy.ToggleDropBomb();
+            }
+
+            if (controller.IsKeyDown(PlayerKeys.RcDitonate))
+            {
+                proxy.ToggleRemoteControl();
+            }
+
+            proxy.MoveTo(position.X, position.Y);
+        }
+    }
+}
---
+++ UWP/MrBoom/ClientSpriteGraphics.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MrBoom.Core.Sprite;
+using MrBoom.Core.Sprites;
+using MrBoom.Core.Terrain;
+
+namespace MrBoom
+{
+    public class ClientSpriteGraphics : IServerGameEntity, IClientDrawableGameEntity
+    {
+        private readonly ISpritePositionProvider position;
+        private readonly ISpriteAnimationProvider animationProvider;
+        private readonly ISpriteProxy proxy;
+
+        private int blinking = 0;
+        private readonly Assets.MovingSpriteAssets animations;
+
+        public ClientSpriteGraphics(ISpritePositionProvider position,
+                                    ISpriteAnimationProvider animationProvider,
+                                    ISpriteProxy proxy,
+                                    Assets assets)
+        {
+            this.position = position;
+            this.animationProvider = animationProvider;
+            this.proxy = proxy;
+
+            if (proxy.Type == SpriteType.Monster)
+            {
+                animations = assets.Monsters[proxy.SubType];
+            }
+            else
+            {
+                animations = assets.Players[proxy.SubType];
+            }
+        }
+
+        public void ServerUpdate()
+        {
+            blinking++;
+        }
+
+        public void Draw(SpriteBatch ctx)
+        {
+            if (animationProvider.FrameIndex != -1)
+            {
+                Color color = Color.White;
+
+                AnimatedImage animation = animations.Normal[animationProvider.AnimateIndex];
+                if (proxy.HasUnplugin && blinking % 30 < 15)
+                {
+                    animation = animations.Ghost[animationProvider.AnimateIndex];
+                }
+                if (proxy.HasSkull && blinking % 30 > 15)
+                {
+                    animation = animations.Red[animationProvider.AnimateIndex];
+                }
+
+                Image img = animation[animationProvider.FrameIndex / 20];
+
+                int x = position.X + 8 + 8 - img.Width / 2;
+                int y = position.Y + 16 - img.Height;
+
+                if (animationProvider.AnimateIndex != 4 || animationProvider.FrameIndex / 20 < animations.Normal[4].Length)
+                {
+                    img.Draw(ctx, x, y, color);
+                }
+            }
+        }
+    }
+}
