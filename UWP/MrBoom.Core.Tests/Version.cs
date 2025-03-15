d3c74cff2b501b488ddb9fc309f28bb45f0ea704
Timofei Zhakov <tima@chemodax.net>
2025-03-15 11:29:12 +0100

    add tests on the core

---
+++ UWP/MrBoom.Core.Tests/Version.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+namespace MrBoom
+{
+    public static class Version
+    {
+        public static readonly string VersionString = "MrBoom Server unknown.unknown.unknown";
+    }
+}
