1c6c486201eb62a4e87552bec4e57ec92b5b0f96
Timofei Zhakov <tima@chemodax.net>
2025-03-15 11:30:07 +0100

    implementing the service provider and their tests

---
+++ UWP/MrBoom.Core.Tests/ServiceProviderTests.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+namespace MrBoom.Core.Tests
+{
+    [TestFixture]
+    public class ServiceProviderTests
+    {
+        private interface ITestSingleton1
+        {
+            public string PublicSigma { get; }
+        }
+
+        private class TestSingleton1 : ITestSingleton1
+        {
+            public string PublicSigma => "TestSingleton1";
+        }
+
+        private class TestSingleton2
+        {
+            private readonly TestSingleton1 testSingleton1;
+
+            public TestSingleton2(TestSingleton1 testSingleton1)
+            {
+                this.testSingleton1 = testSingleton1;
+            }
+
+            public string PublicSigma => testSingleton1.PublicSigma + "qqq";
+        }
+
+        private class TestModule1
+        {
+            public TestModule1(TestSingleton1 testSingleton1)
+            {
+            }
+        }
+
+        [Test]
+        public void SimpleFactory()
+        {
+            ServiceProvider services = new ServiceProvider();
+
+            services.AddSingleton(services => new TestSingleton1());
+            services.AddSingleton(services => new TestSingleton2(services.GetService<TestSingleton1>()));
+
+            var service1 = services.GetService<TestSingleton1>();
+            var service2 = services.GetService<TestSingleton2>();
+
+            Assert.AreEqual("TestSingleton1", service1.PublicSigma);
+            Assert.AreEqual("TestSingleton1qqq", service2.PublicSigma);
+        }
+
+        [Test]
+        public void DependencyInjection()
+        {
+            ServiceProvider services = new ServiceProvider();
+
+            services.AddSingleton<TestSingleton1>();
+            services.AddSingleton<TestSingleton2>();
+
+            var service1 = services.GetService<TestSingleton1>();
+            var service2 = services.GetService<TestSingleton2>();
+
+            Assert.AreEqual("TestSingleton1", service1.PublicSigma);
+            Assert.AreEqual("TestSingleton1qqq", service2.PublicSigma);
+        }
+
+        [Test]
+        public void InterfaceTest()
+        {
+            ServiceProvider services = new ServiceProvider();
+
+            services.AddSingleton<TestSingleton1>();
+
+            var service1 = services.GetService<ITestSingleton1>();
+
+            Assert.AreEqual("TestSingleton1", service1.PublicSigma);
+        }
+    }
+}
---
+++ UWP/MrBoom.Core/ServiceProvider.cs
@@ -0,0 +1,121 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MrBoom.Core
+{
+    public class ServiceProvider
+    {
+        private class Service
+        {
+            public readonly Type type;
+            public readonly Func<ServiceProvider, object> implementationFactory;
+            public object instance;
+
+            public Service(Type type, Func<ServiceProvider, object> implementationFactory)
+            {
+                this.type = type;
+                this.implementationFactory = implementationFactory;
+                instance = null;
+            }
+
+            public Service(Type type, object instance)
+            {
+                this.type = type;
+                this.instance = instance;
+            }
+
+            public object GetService(ServiceProvider serviceProvider)
+            {
+                if (instance == null)
+                {
+                    instance = implementationFactory.Invoke(serviceProvider);
+                }
+
+                return instance;
+            }
+
+            public bool IsCompatibleFor(Type type)
+            {
+                return type.IsAssignableFrom(this.type);
+            }
+        }
+
+        private List<Service> services;
+
+        public ServiceProvider()
+        {
+            services = new List<Service>();
+            AddSingleton(this);
+        }
+
+        public void AddSingleton<T>(Func<ServiceProvider, T> implementationFactory) where T : class
+        {
+            services.Add(new Service(typeof(T), services => implementationFactory(services)));
+        }
+
+        public void AddSingleton<T>(T instance) where T : class
+        {
+            if (instance == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                services.Add(new Service(typeof(T), instance));
+            }
+        }
+
+        public void AddSingleton<T>() where T : class
+        {
+            AddSingleton(services =>
+            {
+                ConstructorInfo constructor = typeof(T).GetConstructors().First();
+                ParameterInfo[] parameters = constructor.GetParameters();
+
+                List<object> args = new List<object>(parameters.Length);
+
+                foreach (ParameterInfo param in parameters)
+                {
+                    args.Add(GetService(param.ParameterType));
+                }
+
+                object obj = constructor.Invoke(args.ToArray());
+
+                return (T)obj;
+            });
+        }
+
+        public IEnumerable<object> EnumerateServices(Type type)
+        {
+            foreach (Service service in services)
+            {
+                if (service.IsCompatibleFor(type))
+                {
+                    yield return service.GetService(this);
+                }
+            }
+        }
+
+        public IEnumerable<T> EnumerateServices<T>()
+        {
+            foreach (object service in EnumerateServices(typeof(T)))
+            {
+                yield return (T)service;
+            }
+        }
+
+        public object GetService(Type type)
+        {
+            return EnumerateServices(type).FirstOrDefault();
+        }
+
+        public T GetService<T>()
+        {
+            return (T)GetService(typeof(T));
+        }
+    }
+}
