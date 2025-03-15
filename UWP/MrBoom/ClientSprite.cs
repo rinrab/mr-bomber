e466e7c38737ca4cdfa8c2f7832e93e6d807fe75
Timofei Zhakov <tima@chemodax.net>
2025-03-15 18:28:13 +0100

    make ClientSprite and ClientSpriteLocalHuman as SpriteBase with a bunch of modules added

--- UWP/MrBoom/ClientSprite.cs
+++ UWP/MrBoom/ClientSprite.cs
@@ -1,8 +1,7 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MrBoom.Core.Sprite;
+using MrBoom.Core.Sprites;
 using MrBoom.Core.Terrain;
 
 namespace MrBoom
@@ -12,76 +11,18 @@ namespace MrBoom
         void Draw(SpriteBatch ctx);
     }
 
-    public interface IClientSprite : IClientGameEntity, IClientDrawableGameEntity, ISprite
+    public class ClientSprite : SpriteBase
     {
-    }
-
-    public class ClientSprite : IClientSprite
-    {
-        private readonly ISpriteProxy proxy;
-        private readonly Assets.MovingSpriteAssets animations;
-
-        public int X { get => proxy.X; }
-        public int Y { get => proxy.Y; }
-
-        public int AnimateIndex => proxy.AnimateIndex;
-        public int FrameIndex => proxy.FrameIndex;
-
-        public Feature Features => proxy.Features;
-        public SkullType? Skull => proxy.Skull;
-
-        public int LifeCount => proxy.LifeCount;
-
-        public bool HasUnplugin => proxy.HasUnplugin;
-        public bool HasSkull => proxy.HasSkull;
-
-        private int blinking = 0;
-
         public ClientSprite(ISpriteProxy proxy, Assets assets)
         {
-            this.proxy = proxy;
+            AddSingleton(proxy);
+            AddSingleton(assets);
 
-            if (proxy.Type == SpriteType.Monster)
-            {
-                animations = assets.Monsters[proxy.SubType];
-            }
-            else
-            {
-                animations = assets.Players[proxy.SubType];
-            }
-        }
+            AddSingleton<ProxyPositionProvider>();
+            AddSingleton<ProxyAnimationProvider>();
+            AddSingleton<SpriteProxyProvider>();
 
-        public void ClientUpdate()
-        {
-            blinking++;
-        }
-
-        public void Draw(SpriteBatch ctx)
-        {
-            if (proxy.FrameIndex != -1)
-            {
-                Color color = Color.White;
-
-                AnimatedImage animation = animations.Normal[proxy.AnimateIndex];
-                if (proxy.HasUnplugin && blinking % 30 < 15)
-                {
-                    animation = animations.Ghost[proxy.AnimateIndex];
-                }
-                if (proxy.HasSkull && blinking % 30 > 15)
-                {
-                    animation = animations.Red[proxy.AnimateIndex];
-                }
-
-                Image img = animation[proxy.FrameIndex / 20];
-
-                int x = X + 8 + 8 - img.Width / 2;
-                int y = Y + 16 - img.Height;
-
-                if (proxy.AnimateIndex != 4 || proxy.FrameIndex / 20 < animations.Normal[4].Length)
-                {
-                    img.Draw(ctx, x, y, color);
-                }
-            }
+            AddSingleton<ClientSpriteGraphics>();
         }
     }
 }
--- UWP/MrBoom/ClientSpriteLocalHuman.cs
+++ UWP/MrBoom/ClientSpriteLocalHuman.cs
@@ -1,95 +1,35 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
-using System;
-using Microsoft.Xna.Framework.Graphics;
-using MrBoom.Core.Sprite;
+using MrBoom.Core.Sprites;
 using MrBoom.Core.Terrain;
 
 namespace MrBoom
 {
-    public class ClientSpriteLocalHuman : Sprite, IClientSprite
+    public class ClientSpriteLocalHuman : SpriteBase
     {
-        private readonly IPlayerProxy proxy;
-        private readonly IClientSprite client;
-        public readonly IController Controller;
-
-        public override bool HasUnplugin => proxy.HasUnplugin;
-        public override bool HasSkull => proxy.HasSkull;
-
-        public override SpriteType Type => proxy.Type;
-        public override int SubType => proxy.SubType;
-
         public ClientSpriteLocalHuman(ITerrainProxy terrain,
                                       IPlayerProxy proxy,
                                       Assets assets,
-                                      IController controller) : base(terrain, proxy.X, proxy.Y, 3)
+                                      IController controller)
         {
-            this.proxy = proxy;
-            client = new ClientSprite(this, assets);
-            Controller = controller;
-        }
+            AddSingleton(terrain);
+            AddSingleton(proxy);
+            AddSingleton(assets);
+            AddSingleton(controller);
 
-        private Directions? GetDirection()
-        {
-            if (Controller.IsKeyDown(PlayerKeys.Up))
-            {
-                return Directions.Up;
-            }
-            else if (Controller.IsKeyDown(PlayerKeys.Left))
-            {
-                return Directions.Left;
-            }
-            else if (Controller.IsKeyDown(PlayerKeys.Right))
-            {
-                return Directions.Right;
-            }
-            else if (Controller.IsKeyDown(PlayerKeys.Down))
-            {
-                return Directions.Down;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            AddSingleton(new SpriteStartInfo(0, 0, 3, 1, proxy.Type, proxy.SubType));
 
-        public void ClientUpdate()
-        {
-            Direction = GetDirection();
+            AddSingleton<SpritePosition>();
+            AddSingleton<NullBombKicker>(); // TODO:
+            AddSingleton<SpriteAnimationController>();
+            AddSingleton<SpriteSpeedProvider>();
+            AddSingleton<SpriteMovementController>();
 
-            Features = proxy.Features;
-            Skull = proxy.Skull;
+            AddSingleton<ProxyAnimationProvider>();
+            AddSingleton<ProxyEffectProvider>();
 
-            if (Math.Abs(X - proxy.X) + Math.Abs(Y - proxy.Y) > 16)
-            {
-                MoveTo(proxy.X, proxy.Y);
-            }
-
-            base.ServerUpdate();
-
-            if (Controller.IsKeyDown(PlayerKeys.Bomb))
-            {
-                proxy.ToggleDropBomb();
-            }
-
-            if (Controller.IsKeyDown(PlayerKeys.RcDitonate))
-            {
-                proxy.ToggleRemoteControl();
-            }
-
-            proxy.MoveTo(X, Y);
-
-            client.ClientUpdate();
-        }
-
-        public void Draw(SpriteBatch ctx)
-        {
-            client.Draw(ctx);
-        }
-
-        public override void KickBomb(int x, int y, int dx, int dy)
-        {
-            // nothing to kick yet
+            AddSingleton<ClientSpriteController>();
+            AddSingleton<ClientSpriteGraphics>();
         }
     }
 }
