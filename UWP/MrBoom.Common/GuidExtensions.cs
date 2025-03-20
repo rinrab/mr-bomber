5559573cce6b7cf7a3129cf600e9b0bb1169887e
Timofei Zhakov <tima@chemodax.net>
2025-03-20 11:44:52 +0100

    implement Guid.ToFriendlyString()

---
+++ UWP/MrBoom.Common/GuidExtensions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+
+namespace MrBoom.Common
+{
+    public static class GuidExtensions
+    {
+        public static string ToFriendlyString(this Guid guid)
+        {
+            return guid.ToString("N").Substring(0, 7);
+        }
+    }
+}
