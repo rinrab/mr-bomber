c730bcaa4158ed084c76fad7938b99f4fa2a8e1e
Timofei Zhakov <tima@chemodax.net>
2025-03-20 11:49:09 +0100

    implement lobby details page

---
+++ UWP/MrBoom.Server/Admin/Lobby.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MrBoom.Server.Admin
+{
+    public class LobbyModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public Guid Id { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
