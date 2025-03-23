d2d80766994ec201e0ba7d40fff548918e712bc2
Timofei Zhakov <tima@chemodax.net>
2025-03-23 18:11:40 +0100

    put die animation using the ProxyHealthProvider when we die

--- UWP/MrBoom.Core/Sprites/Proxy/ProxyHealthProvider.cs
+++ UWP/MrBoom.Core/Sprites/Proxy/ProxyHealthProvider.cs
@@ -1,22 +1,34 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
+using MrBoom.Core.Sprites.Controllers;
 using MrBoom.Core.Sprites.Interface;
 using MrBoom.Core.Terrain;
 
 namespace MrBoom.Core.Sprites.Proxy
 {
-    public class ProxyHealthProvider : IHealthProvider
+    public class ProxyHealthProvider : IHealthProvider, IServerGameEntity
     {
         private readonly ISpriteProxy proxy;
+        private readonly SpriteAnimationController animation;
 
         public bool IsDie => proxy.IsDie;
         public bool IsAlive => !IsDie;
 
         public bool HasUnplugin => proxy.HasUnplugin;
 
-        public ProxyHealthProvider(ISpriteProxy proxy)
+        public ProxyHealthProvider(ISpriteProxy proxy,
+                                   SpriteAnimationController animation)
         {
             this.proxy = proxy;
+            this.animation = animation;
+        }
+
+        public void ServerUpdate()
+        {
+            if (IsDie)
+            {
+                animation.SetAnimation(4);
+            }
         }
     }
 }
