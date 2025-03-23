3c9e5198c9a909011b16e32ef3990aeec6e06ac8
Timofei Zhakov <tima@chemodax.net>
2025-03-23 18:22:44 +0100

    add ProxyEffectProvider to ClientSprite

--- UWP/MrBoom/Game/ClientSprite.cs
+++ UWP/MrBoom/Game/ClientSprite.cs
@@ -21,6 +21,7 @@ namespace MrBoom
 
             AddSingleton<ProxyPositionProvider>();
             AddSingleton<ProxyAnimationProvider>();
+            AddSingleton<ProxyEffectProvider>();
             AddSingleton<ProxyHealthProvider>();
 
             AddSingleton<ClientSpriteGraphics>();
